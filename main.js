5bcb0d6d649e2e24755000f89c176098fdcf61ae
michaelprys <musha09cz@gmail.com>
2024-01-13 14:20:41 +0100

    slider

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 import './src/css/main.css';
 import './src/css/decorations.css';
+import './src/modules/slider';
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,1 +1,13 @@
+import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        // autoplay: true,
+        arrows: false,
+        gap: '1.5em',
+        padding: { left: '4rem', right: '4rem' },
+    });
+    splide.mount();
+});
